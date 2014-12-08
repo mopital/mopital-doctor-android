eb9087a3ed473d6bd0ef022a08cfd8b40d3ec903
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2014-12-08 21:17:35 +0200

    first commit

---
+++ app/src/main/java/client/android/paying/com/mopitaldoctor/activities/MainActivity.java
@@ -0,0 +1,47 @@
+package client.android.paying.com.mopitaldoctor.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import client.android.paying.com.mopitaldoctor.R;
+
+
+public class MainActivity extends Activity {
+
+    @InjectView(R.id.text)
+    TextView textview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        textview.setText("Butter Knife sample");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
