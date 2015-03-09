5f753734a65e0ad83fb1b94ecf2bb15d0d7aabb8
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2015-03-09 23:52:39 +0200

    quick fix

--- app/src/main/java/com/mopital/doctor/activities/SignUpActivity.java
+++ app/src/main/java/com/mopital/doctor/activities/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.mopital.doctor.activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -14,7 +15,6 @@ import com.mopital.doctor.R;
 import com.mopital.doctor.core.DefaultServerApi;
 import com.mopital.doctor.core.ServerApiProvider;
 import com.mopital.doctor.core.volley.responses.Result;
-import com.mopital.doctor.models.Patient;
 
 public class SignUpActivity extends ActionBarActivity {
 
@@ -89,18 +89,23 @@ public class SignUpActivity extends ActionBarActivity {
             }
         );
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// TODO Auto-generated method stub
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			setResult(RESULT_CANCELED);
-			finish();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
