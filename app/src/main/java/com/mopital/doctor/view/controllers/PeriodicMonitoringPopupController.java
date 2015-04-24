52b87bf931a1a1a7b0bc3bb4fab5d4ee1ef89d7f
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2015-04-24 16:56:50 +0300

    popup controllers

---
+++ app/src/main/java/com/mopital/doctor/view/controllers/PeriodicMonitoringPopupController.java
@@ -0,0 +1,86 @@
+package com.mopital.doctor.view.controllers;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.mopital.doctor.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ahmetkucuk on 24/04/15.
+ */
+public class PeriodicMonitoringPopupController {
+
+    private Context context;
+
+    private Dialog dialog = null;
+    private View dialogView = null;
+    private WarningPopupViewHolder warningPopupViewHolder;
+
+    public PeriodicMonitoringPopupController(Context context) {
+        this.context = context;
+
+        dialog = new Dialog(context);
+        dialogView = LayoutInflater.from(context).inflate(R.layout.popup_patient_record_detail, null);
+        ((TextView)dialogView.findViewById(R.id.popup_header_textview)).setText("Periodic Monitoring");
+        dialog.requestWindowFeature(Window.FEATURE_PROGRESS);
+        dialog.setContentView(dialogView);
+    }
+
+    public void showPopup(List data) {
+
+        if(data == null || data.size() == 0) {
+            if(dialog.isShowing()) {
+                dialog.dismiss();
+            }
+            return;
+        }
+
+        if(warningPopupViewHolder == null) {
+            warningPopupViewHolder = new WarningPopupViewHolder(dialogView);
+        }
+
+        warningPopupViewHolder.getWarningListView().setAdapter(null);
+        warningPopupViewHolder.getCloseWarningsButton().setOnClickListener(onCloseWarningClickListener);
+
+        dialog.show();
+
+
+    }
+
+    private View.OnClickListener onCloseWarningClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(dialog != null)
+                dialog.dismiss();
+        }
+    };
+
+    static class WarningPopupViewHolder {
+
+        private ListView warningListView;
+        private Button closeWarningsButton;
+
+        public WarningPopupViewHolder(View view) {
+            warningListView = (ListView)view.findViewById(R.id.popup_listview);
+            closeWarningsButton = (Button)view.findViewById(R.id.close_dialog_button);
+        }
+
+        public ListView getWarningListView() {
+            return warningListView;
+        }
+
+        public Button getCloseWarningsButton() {
+            return closeWarningsButton;
+        }
+    }
+
+}
